d89a4b9e62bb1ced4c4a8e465c6655deae9a870f
Ankit Kumar <ankitgeotek@gmail.com>
2023-11-03 15:08:11 +0530

    first

---
+++ 02_linear_Algebra.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a=np.array([1,2,3,4,5])
+
+distance=np.linalg.norm(a)
+print (f'Distance form origin of vector A is {distance}')
+
+
+
+A=np.array([1,2,3,4,5])
+B=np.array([6,7,8,9,10])
+
+difference=A-B
+
+print(difference)
+
+
+print (f'Distance form origin of vector A is {distance}')
---
+++ 03_tensor.py
@@ -0,0 +1,12 @@
+import numpy as np
+Tensor_0d=np.array(2)
+print(Tensor_0d)
+print(Tensor_0d.ndim)
+
+Tensor_1d=np.array([1,2,3])
+print(Tensor_1d)
+print(Tensor_1d.ndim)
+
+tensor_2d=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(tensor_2d)
+print(tensor_2d.ndim)
